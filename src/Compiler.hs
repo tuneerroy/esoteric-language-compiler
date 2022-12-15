e1715a1d6800abc53fdf235dc9d5c2afe7ab4d00
Richard Chai <richardchai@Richards-MacBook-Pro-2.local>
2022-12-15 03:24:31 -0500

    janky compiler

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -14,97 +14,145 @@ import WSyntax
 compileCommand :: (Eq a, NonWhitespaceShow a) => WInstruction a -> [String]
 compileCommand i = case i of
   InputChar ->
-    [ "; input character",
-      "ldr r0, =charFormat",
-      "ldr r1, =inputRead",
-      "bl scanf",
-      "sub sp, sp, #4",
-      "str r1, [sp]"
+    [ "TODO"
     ]
   InputNum ->
-    [ "; input number",
-      "ldr r0, =numFormat",
-      "ldr r1, =inputRead",
-      "bl scanf",
-      "sub sp, sp, #4",
-      "str r1, [sp]"
+    [ "TODO"
     ]
   OutputChar ->
-    [ "; output character",
-      "mov r0, sp",
-      "bl printf"
+    [ "// outputChar",
+      "mov x0, #1",
+      "adrp x1, buf@page",
+      "add x1, x1, buf@pageoff",
+      "ldr x8, [sp]",
+      "str x8, [x1]",
+      "mov x2, #1",
+      "mov x16, #4",
+      "svc #0x80"
     ]
   OutputNum ->
-    [ "; output number",
-      "mov r0, sp",
-      "bl printf"
+    -- TODO fix labels
+    [ "// outputNum",
+      "ldr x0, [sp]",
+      "_int_to_ascii1:",
+      "cmp x0, #0",
+      "b.ge _int_to_ascii_pos1",
+      "mov x1, #45 // ascii for '-'",
+      "str x1, [sp, #-16]!",
+      "mov x27, x0",
+      "bl _ouput_char",
+      "ldr x28, [sp], #16",
+      "mov x0, x27",
+      "mov x27, #0",
+      "negs x0, x0",
+      "_int_to_ascii_pos1:",
+      "// expects x0 = int",
+      "cmp x0, #10",
+      "b.lt _int_to_ascii_br1",
+      "mov x1, #10             // if x0 >= 10",
+      "bl _divide",
+      "mov x0, x2",
+      "add x3, x3, #48",
+      "str x3, [sp, #-16]!",
+      "add x27, x27, #1",
+      "bl _int_to_ascii_pos1",
+      "_int_to_ascii_br1:      // if x0 < 10",
+      "add x8, x0, #48         // add ascii representation of x0 to stack",
+      "str x8, [sp, #-16]!",
+      "add x27, x27, #1",
+      "// print the stack x27 times",
+      "_print_stack1:",
+      "cmp x27, #0             // if x27 <= 0",
+      "b.le _done_ascii1",
+      "bl _ouput_char          // print top of stack",
+      "ldr x28, [sp], #16",
+      "sub x27, x27, #1",
+      "bl _print_stack1",
+      "_done_ascii1:"
     ]
   Push n ->
-    [ "; push",
-      "mov r0, #" ++ show n,
-      "sub sp, sp, #4",
-      "str r0, [sp]"
+    [ "// push",
+      "mov x0, #" ++ show n,
+      "str x0, [sp, #-16]!"
     ]
   Dup ->
-    [ "; dup",
-      "ldr r1, [sp]",
-      "sub sp, sp, #4",
-      "str r1, [sp]"
+    [ "// dup",
+      "ldr x0, [sp], #16",
+      "str x0, [sp, #-16]!",
+      "str x0, [sp, #-16]!"
     ]
   Swap ->
-    [ "; swap",
-      "ldr r0, [sp]",
-      "add, sp, sp, #4",
-      "ldr r1, [sp]",
-      "str r0, [sp]",
-      "sub sp, sp, #4",
-      "ldr r0, [sp]"
+    [ "// swap",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "str x0, [sp, #-16]!",
+      "str x1, [sp, #-16]!"
     ]
   Discard ->
-    [ "; discard",
-      "add sp, sp, #4"
+    [ "// discard",
+      "ldr x0, [sp], #16"
     ]
   Copy n ->
-    -- assumed top down nth
-    [ "; copy",
-      "move r1 #" ++ show (n * 4), -- offset
-      "ldr r2, [sp, r1]",
-      "sub sp, sp, #4",
-      "str r2, [sp]"
+    [ "// copy",
+      "ldr x0, [sp, #" ++ show ((n - 1) * 16) ++ "]", -- offset
+      "str x0, [sp, #-16]!"
     ]
   Slide n ->
-    [ "; slide",
-      "add sp, sp, #" ++ show (n * 4)
+    -- TODO fix labels
+    [ "// slide",
+      "mov x0, #" ++ show n,
+      "ldr x1, [sp], #16",
+      "slide_loop1:",
+      "cmp x0, #0",
+      "b.le slide1",
+      "ldr x28, [sp], #16",
+      "sub x0, x0, #1",
+      "bl slide_loop1",
+      "slide1:",
+      "tr x1, [sp, #-16]!"
     ]
   Arith Add ->
-    [ "; add arith",
-      "ldr r0, [sp]",
-      "add, sp, sp, #4",
-      "ldr r1, [sp]",
-      "add r1, r1, r0",
-      "str r1, [sp]"
+    [ "// add arith",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "add x2, x0, x1",
+      "str x2, [sp, #-16]!"
       -- not sure if these are for signed/unsigned operationz
     ]
   Arith Sub ->
-    [ "; sub arith",
-      "ldr r0, [sp]",
-      "add, sp, sp, #4",
-      "ldr r1, [sp]",
-      "sub r1, r1, r0",
-      "str r1, [sp]"
-      -- not sure if these are for signed/unsigned operationz
+    [ "// sub arith",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "sub x2, x0, x1",
+      "str x2, [sp, #-16]!"
     ]
-  Arith wb ->
-    [ -- TODO: handle division & mod, -- ask Joe
-      "; arith",
-      "sub sp, sp, #4"
+  Arith Mul ->
+    [ "// mul arith",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "mul x2, x0, x1",
+      "str x2, [sp, #-16]!"
+    ]
+  Arith Div ->
+    [ "// div arith",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "bl _divide",
+      "str x2, [sp, #-16]!"
+    ]
+  Arith Mod ->
+    [ "// mod arith",
+      "ldr x0, [sp], #16",
+      "ldr x1, [sp], #16",
+      "bl _divide",
+      "str x3, [sp, #-16]!"
     ]
   Label a ->
-    [ "; label",
+    [ "// label",
       toString a ++ ":"
     ]
   Call a ->
-    [ "; call",
+    [ "// call",
       "bl " ++ toString a
     ]
   Branch Any a ->
@@ -148,22 +196,30 @@ compileCommand i = case i of
 
 ioPrep :: [String]
 ioPrep =
-  [ ".global printf",
-    ".global scanf",
-    ".data",
-    ".align 2",
-    "inputRead: .word 0",
-    ".align 2",
-    "numFormat: .asciz \"%d\"",
-    ".align 2",
-    "charFormat: .asciz \"%c\"",
+  [ ".data",
+    "buf: .ds 4 // memory buffer for IO",
+    ".text",
     ".global _start",
     ".align 2",
+    "_divide:",
+    "udiv x2, x0, x1",
+    "msub x3, x2, x1, x0",
+    "ret",
+    "_ouput_char:",
+    "mov x0, #1",
+    "adrp x1, buf@page",
+    "add x1, x1, buf@pageoff",
+    "ldr x8, [sp]",
+    "str x8, [x1]",
+    "mov x2, #1",
+    "mov x16, #4",
+    "svc #0x80",
+    "ret",
     "_start:"
   ]
 
 compileProgram :: (Eq a, NonWhitespaceShow a) => [WInstruction a] -> [String]
-compileProgram a = ioPrep ++ concatMap compileCommand a ++ ["end:"]
+compileProgram a = ioPrep ++ concatMap compileCommand a ++ ["end:", "mov x0, #0", "mov x16, #1", "svc #0x80"]
 
 class NonWhitespaceShow a where
   toString :: a -> String
