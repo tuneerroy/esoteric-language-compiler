1e73b6a66f0729a325a0130258a95c4bab78c60f
Richard Chai <rich.chai6@gmail.com>
2022-12-18 23:17:10 -0500

    updated main

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import ASyntax (toArm64String)
 -- import Compiler (compileProgram)
+
+import BCompiler (compileProgram)
+import BParser (bParseString)
 import Control.Monad (void, when)
 import Data.List (intercalate)
 import Data.Maybe (fromMaybe)
@@ -27,8 +30,13 @@ main = do
           writeFile outputFilename compiled
             >>= return (putStrLn "Compiled program")
 
+-- tryOFileConversion :: String -> Maybe String
+-- tryOFileConversion (c : ".ws") = Just $ c : ".s"
+-- tryOFileConversion (c : cs) = tryOFileConversion cs >>= (\cs' -> return (c : cs'))
+-- tryOFileConversion _ = Nothing
+
 tryOFileConversion :: String -> Maybe String
-tryOFileConversion (c : ".ws") = Just $ c : ".s"
+tryOFileConversion (c : ".b") = Just $ c : ".s"
 tryOFileConversion (c : cs) = tryOFileConversion cs >>= (\cs' -> return (c : cs'))
 tryOFileConversion _ = Nothing
 
@@ -39,7 +47,9 @@ tryOFileConversion _ = Nothing
 
 compile :: String -> Maybe String
 compile s = do
-  commands <- wParseString s
-  let assembly = WCompiler.compileProgram commands
+  -- commands <- wParseString s
+  -- let assembly = WCompiler.compileProgram commands
+  commands <- bParseString s
+  let assembly = BCompiler.compileProgram commands
   let assemblyStr = map toArm64String assembly
   return (intercalate "\n" assemblyStr)
